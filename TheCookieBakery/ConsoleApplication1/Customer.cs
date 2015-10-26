ec97fd75b9fa8bb23f26ae5fbc0d1e4aca0febc3
TanjaRio <tanjario@hotmail.no>
2015-10-26 16:38:04 +0100

    CookieFactory

--- TheCookieBakery/ConsoleApplication1/Customer.cs
+++ TheCookieBakery/ConsoleApplication1/Customer.cs
@@ -8,5 +8,7 @@ namespace NewBakery
 {
     class Customer
     {
+
+        
     }
 }
--- TheCookieBakery/ConsoleApplication1/Program.cs
+++ TheCookieBakery/ConsoleApplication1/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace NewBakery
@@ -10,6 +11,7 @@ namespace NewBakery
     {
         static void Main(string[] args)
         {
-        }3
+            
+        }
     }
 }
